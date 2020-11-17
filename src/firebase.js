8c8cfb14df915bdc9e58d2a0064e2187e5425e24
georgejeswin <jeswinmyladoor@gmail.com>
2020-11-17 22:12:44 +0530

    database

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,16 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBbKuD-eDRvId-AwEYVnCBAD6x3zj8vB38",
-  authDomain: "ig-reels-clone-8a3c4.firebaseapp.com",
-  databaseURL: "https://ig-reels-clone-8a3c4.firebaseio.com",
-  projectId: "ig-reels-clone-8a3c4",
-  storageBucket: "ig-reels-clone-8a3c4.appspot.com",
-  messagingSenderId: "99466663759",
-  appId: "1:99466663759:web:72fea78030cda6ef36973a",
-  measurementId: "G-S6H22QQ8CT",
+  apiKey: "AIzaSyAeDw_7m1dUqKph4A003ztwvUTzUuk5Kuw",
+  authDomain: "instagram-reels-clone-24632.firebaseapp.com",
+  databaseURL: "https://instagram-reels-clone-24632.firebaseio.com",
+  projectId: "instagram-reels-clone-24632",
+  storageBucket: "instagram-reels-clone-24632.appspot.com",
+  messagingSenderId: "903936977921",
+  appId: "1:903936977921:web:8ceababeea7cc299732400",
+  measurementId: "G-35EHJHMKZM",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
